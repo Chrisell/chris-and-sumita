89f6f5f341f2bedae56f8e223e6bddb4bfd60fea
Ninikakes <88406816+Ninikakes@users.noreply.github.com>
2021-08-06 08:04:40 -0400

    Added support email

--- src/Rsvp.js
+++ src/Rsvp.js
@@ -52,6 +52,7 @@ class Rsvp extends React.Component {
                     <Col>
                         <p>We cannot wait to finELLy celebrate with you! As this event is highly anticipated, we wanted to ensure everyone feels safe by requiring all guests over the age of 12 to provide proof of vaccination for the events - which will be confirmed by the event staff. We appreciate you taking the time to create a safer space to celebrate our marriage and a safer community.</p>
                         <p>You can RSVP for our events by visiting <a href=" https://www.theknot.com/us/sumita-routh-and-chris-ell-oct-2021/rsvp" target="_blank">this link</a>. The password is <b>finELLy</b> - which is case sensitive.</p>
+                        <p> If you run into any issues RSVPing on the website, please send an email to <b> sumitanchris@gmail.com </b>.</p>
                     </Col>
                 </Row>
             </Container>
